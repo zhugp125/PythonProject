65ace3679c33edd6997fe80798913b5ba4f583f3
zhugp <18811475054@163.com>
2019-03-16 23:28:36 +0800

    支持m3u8视频嵌套

--- url/download_m3u8.py
+++ url/download_m3u8.py
@@ -15,6 +15,9 @@ def get(text, pattern):
     obj = re.search(pattern, text, re.M)
     return obj.group(1)
 
+def find(text, pattern):
+    return re.search(pattern, text, re.M) is not None
+
 def parse(text, pattern):
     for iter in re.finditer(pattern, text, re.M):
         yield iter.group()[1:]
@@ -33,28 +36,35 @@ if __name__ == '__main__':
     # 获取m3u8的地址
     url = get(text, r"video: '(.*)'")
     # 获取视频的名称
-    filename = get(text, r'<h6 class="font-20 title">(.*)</h6>')
+    filename = get(text, r'<title>(.*)</title>')
     #print('url: {}\nfile: {}'.format(url, filename))
 
-    req = download(url)
-    if not req.text.startswith('#EXTM3U'):
-        raise BaseException('m3u8 error url')
+    while True:
+        text = download(url).text
+        #print('url_____________', url)
+        if not text.startswith('#EXTM3U'):
+            raise BaseException('m3u8 error url')
 
-    url_pre = url.rsplit('/', 1)[0]
-    #print('url', url_pre)
+        if find(text, r'#EXT-X-STREAM-INF'):
+            url = os.path.join(url.rsplit('/', 1)[0], get(text, r'(.*).m3u8') + '.m3u8')
+        else:
+            break
+
     # 加密类型
-    encry = get(req.text, r'#EXT-X-KEY:METHOD=(.*),URI=".*"')
+    encry = get(text, r'#EXT-X-KEY:METHOD=(.*),URI=".*"')
     # key
-    key = get(req.text, r'#EXT-X-KEY:METHOD=.*,URI="(.*)"')
+    key = get(text, r'#EXT-X-KEY:METHOD=.*,URI="(.*)"')
     #print('encry: {}, key: {}'.format(encry, key))
     # 密钥
+    url_pre = url.rsplit('/', 1)[0]
+    #print('url............. ', url_pre)
     key_data = download(os.path.join(url_pre, key)).content
     #print('key data: ', key_data)
     cryptor = AES.new(key_data, AES.MODE_CBC, key_data)
     
     from_file = filename + '.ts'
     with open(from_file, 'wb') as f:
-        for m3u8 in parse(req.text, r'$\n[A-Za-z0-9]+.ts'):
+        for m3u8 in parse(text, r'$\n[A-Za-z0-9]+.ts'):
             m3u8_url = os.path.join(url_pre, m3u8)
             print('url: ', m3u8_url)
             write_url(m3u8_url, cryptor, f)
